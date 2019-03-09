7055d32219c6fefad057a55fd8f5dee429ceed95
Daria <DARIAPIERZGALSKA@gmail.com>
2019-03-09 01:13:50 +0100

    poprawki branch daria

--- src/rekurencja/Tablica.java
+++ src/rekurencja/Tablica.java
@@ -1,4 +1,10 @@
 package rekurencja;
 
+import zadania.Sześcian;
+
 public class Tablica {
+    public static void main(String[] args) {
+        Sześcian sześcian=new Sześcian();
+        String.format("");
+    }
 }
